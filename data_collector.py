84ac3ddf583e3a69fbf317554c32d227787f2734
Ben-Kia <seyedreza.kiamousavi@rwth-aachen.de>
2024-01-13 12:19:11 +0100

    common_dev

--- data_collector.py
+++ data_collector.py
@@ -5,31 +5,23 @@ import openai
 import sys
 sys.path.append('../..')
 
-#import panel as pn  # GUI
-#pn.extension()
-
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
 
 from langchain_openai import ChatOpenAI
 from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
-from langchain.text_splitter import MarkdownHeaderTextSplitter
-from langchain.text_splitter import TokenTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter, MarkdownHeaderTextSplitter, TokenTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceBgeEmbeddings
 from langchain.llms import OpenAI
 from langchain.retrievers.self_query.base import SelfQueryRetriever
 from langchain.chains.query_constructor.base import AttributeInfo
-from langchain.retrievers import SVMRetriever
-from langchain.retrievers import TFIDFRetriever
-from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers import ContextualCompressionRetriever, SVMRetriever, TFIDFRetriever
 from langchain.retrievers.document_compressors import LLMChainExtractor
-from langchain.chains import RetrievalQA
+from langchain.chains import RetrievalQA, ConversationalRetrievalChain
 from langchain.prompts import PromptTemplate
 from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
 
 from transformers import AutoTokenizer
 import numpy as np
@@ -71,7 +63,6 @@ def pretty_print_docs(docs):
 #%% Load docs
 """loads any number of pdfs that is placed in the ./docs directory"""
 
-
 pdf_list = glob.glob('./docs/*.pdf')
 docs = []
 
@@ -84,8 +75,8 @@ for pdf in pdf_list:
 print(f"{len(pdf_list)} file(s) were loaded into {len(docs)} pages.")
 
 # loading into text
-all_page_text=[page.page_content for page in docs]
-joined_page_text=" ".join(all_page_text)
+#all_page_text=[page.page_content for page in docs]
+#joined_page_text=" ".join(all_page_text)
 
 #%% Split into chunks
 splitter = RecursiveCharacterTextSplitter(
@@ -95,7 +86,7 @@ splitter = RecursiveCharacterTextSplitter(
 )
 
 doc_splits = splitter.split_documents(docs)
-text_splits = splitter.split_text(joined_page_text)
+#text_splits = splitter.split_text(joined_page_text)
 
 #%% Store in vectors and embed
 """here you can choose between OpenAI and HuggingFace embeddings"""
@@ -106,15 +97,16 @@ embedding_function = OpenAIEmbeddings()
 
 embedding_function = HuggingFaceBgeEmbeddings(
     model_name="BAAI/bge-base-en-v1.5",
-    model_kwargs={'device': 'cpu'}, # works with CUDA, too, but requires CUDA-enabled torch
+    model_kwargs={'device': 'cuda'}, # works with CUDA, too, but requires CUDA-enabled torch
     encode_kwargs={'normalize_embeddings': True},
     query_instruction="Represent this sentence for searching relevant passages: "
 )  
+"""
 tokenizer = AutoTokenizer.from_pretrained('BAAI/bge-base-en-v1.5')
 text_splitter = RecursiveCharacterTextSplitter.from_huggingface_tokenizer(
     tokenizer, chunk_size=100, chunk_overlap=0
 )
-
+"""
 
 vectordb = Chroma.from_documents(
     doc_splits,
@@ -126,8 +118,8 @@ print(vectordb._collection.count())
 
 """other retrievers are also available"""
 
-svm_retriever = SVMRetriever.from_texts(text_splits, embedding_function)
-tfidf_retriever = TFIDFRetriever.from_texts(text_splits)
+#svm_retriever = SVMRetriever.from_texts(text_splits, embedding_function)
+#tfidf_retriever = TFIDFRetriever.from_texts(text_splits)
 
 
 
@@ -136,8 +128,9 @@ tfidf_retriever = TFIDFRetriever.from_texts(text_splits)
 for example: fitler={"source:"path/to/source"} can be used to limit the search 
 to one specific source"""
 
-question = "what is the preparation process"
+question = "list all the different protone conducting membrane configurations you find in these papers"
 
+"""
 docs_sim_retrieved = vectordb.similarity_search(question,k=3)
 print("The following were retrieved by similarity search:")
 pretty_print_docs(docs_sim_retrieved)
@@ -145,6 +138,7 @@ pretty_print_docs(docs_sim_retrieved)
 docs_mmr_retrieved = vectordb.max_marginal_relevance_search(question,k=3, fetch_k=3)
 print("The following were retrieved by Max Marginal Relevance search:")
 pretty_print_docs(docs_mmr_retrieved)
+"""
 
 #%% Retrieval using a self-query retriever
 """uses an LLM to extract the query string and the metadata filter so you don't 
@@ -165,7 +159,8 @@ metadata_field_info = [
     ),
 ]
 
-document_content_description = "cell performance" # do we need this argument? if so make it more specific.
+# We could use the GPT to summarize each pdf and write an individual description.
+document_content_description = "paper on protonic ceramic cells" # do we need this argument? if so make it more specific.
 
 llm_self_query_ret = OpenAI(model='gpt-3.5-turbo-instruct', temperature=0) # for factual answers
 
@@ -184,7 +179,7 @@ pretty_print_docs(retrieved_docs)
 """when you don't want to pass a large amount of documents through the application,
 you can find the information most relevant to the query instead"""
 
-
+"""
 llm_compression = OpenAI(temperature=0)
 compressor = LLMChainExtractor.from_llm(llm_compression)
 
@@ -195,9 +190,11 @@ compression_retriever = ContextualCompressionRetriever(
 
 compressed_docs = compression_retriever.get_relevant_documents(question)
 pretty_print_docs(compressed_docs)
+"""
 
 #%% Alternative retrievers
 
+"""
 docs_svm=svm_retriever.get_relevant_documents(question)
 print("docs retrieved by SVM retriever")
 pretty_print_docs(docs_svm)
@@ -205,9 +202,10 @@ pretty_print_docs(docs_svm)
 docs_tfidf=tfidf_retriever.get_relevant_documents(question)
 print("docs retrieved by TFIDF retriever")
 pretty_print_docs(docs_tfidf)
+"""
 
 #%% RetrievalQA Chain
-
+"""
 qa_chain = RetrievalQA.from_chain_type(
     llm,
     retriever=vectordb.as_retriever()
@@ -216,11 +214,13 @@ qa_chain = RetrievalQA.from_chain_type(
 result = qa_chain({"query": question})
 
 result["result"]
-
+"""
 
 #%% Prompting
 
 # Build prompt
+
+
 template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer. Use three sentences maximum. Keep the answer as concise as possible. Always say "thanks for asking!" at the end of the answer. 
 {context}
 Question: {question}
@@ -228,9 +228,10 @@ Helpful Answer:"""
 QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
 
 # Run chain
+
 qa_chain = RetrievalQA.from_chain_type(
     llm,
-    retriever=vectordb.as_retriever(),
+    retriever=retriever,#vectordb.as_retriever(),
     return_source_documents=True,
     chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}
 )
@@ -244,14 +245,14 @@ qa_chain_mr = RetrievalQA.from_chain_type(
 )
 """
 """
-qa_chain_mr = RetrievalQA.from_chain_type(
+qa_chain_r = RetrievalQA.from_chain_type(
     llm,
     retriever=vectordb.as_retriever(),
     chain_type="refine" # "map_reduce" or "refine"
 )
 """
 
-result = qa_chain({"query": question})
+result = qa_chain({"query": question}, {"context":retrieved_docs})
 result["result"]
 result["source_documents"][0]
 
